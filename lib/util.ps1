0319c79f2c47c5425c1cdd3c79b8a92f134839ce
orion55 <grol55@mail.ru>
2020-05-29 22:50:21 +0500

    First commit

---
+++ lib/util.ps1
@@ -0,0 +1,68 @@
+﻿function parseXML {
+    param (
+        [string]$xmlFile
+    )
+    $fileName = Split-Path $xmlFile -leaf
+    Write-Log -EntryType Information -Message "Обработка файла $fileName"
+    [xml]$xml = Get-Content $xmlFile
+    
+    $list = $xml.ФайлПФР.ПачкаВходящихДокументов.СПИСОК_НА_ЗАЧИСЛЕНИЕ
+    if (($list | Measure-Object).count -eq 0) {        
+        throw "Информация для парсинга в файле $fileName не найдена!"
+    }
+    
+    $people = @()
+    
+    ForEach ($person in $list.СведенияОполучателе) {
+        $fio = $person.ФИО
+        $fullName = $fio.Фамилия + ' ' + $fio.Имя + ' ' + $fio.Отчество
+        
+        $obj = New-Object psobject            
+        $obj | Add-Member -type noteproperty -name iterator -Value 0            
+        $obj | Add-Member -type noteproperty -name  account -Value $person.НомерСчета
+        $obj | Add-Member -type noteproperty -name  sum -Value $person.СуммаКдоставке
+        $obj | Add-Member -type noteproperty -name  fio -Value $fullName
+
+        $people += $obj        
+    }    
+    return $people
+}
+
+function addIterator {
+    param (
+        $data
+    )
+    
+    for ($i = 0; $i -lt $data.Count; $i++) {        
+        $data[$i].iterator = $i + 1
+    }
+
+    return $data
+}
+
+function exportCSV {
+    param (
+        $data,
+        [string]$fileName
+    )
+    
+    if (($data | Measure-Object).count -eq 0) {
+        throw "Информация в XML-файле не найдена!"        
+    }
+    
+    $data | Export-Csv -Path $fileName -NoTypeInformation -Delimiter "," -Force -Encoding OEM
+    
+    Write-Log -EntryType Information -Message "Сохраняем результат в файл $fileName"
+}
+
+function convertCsv {
+    param (
+        $fileName
+    )
+    $content = Get-Content $fileName | Select-Object -Skip 1
+
+    for ($i = 0; $i -lt $content.Count; $i++) {
+        $content[$i] = $content[$i] -replace '"', ''
+    }
+    $content | Set-Content -Path $fileName
+}
---
+++ pensionFund.ps1
@@ -0,0 +1,62 @@
+﻿#Программа конвертации xml-файлов из Пенсионного фонда в csv-файл
+#(c) Гребенёв О.Е. 29.05.2020
+
+[string]$curDir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+Set-Location $curDir
+[string]$lib = "$curDir\lib"
+$curDate = Get-Date -Format "ddMMyyyy"
+[string]$logName = $curDir + "\log\" + $curDate + "_pension.log"
+
+[string]$inPath = "$curDir\in"
+[string]$outPath = "$curDir\out"
+
+[string]$csvFile = $outPath + "\" + $curDate + ".txt"
+
+. $lib/PSMultiLog.ps1
+. $lib/libs.ps1
+. $lib/util.ps1
+
+Clear-Host
+
+Start-HostLog -LogLevel Information
+Start-FileLog -LogLevel Information -FilePath $logName -Append
+
+#проверяем существуют ли нужные пути и файлы
+testDir(@($inPath, $outPath))
+
+Write-Log -EntryType Information -Message "Начало работы pensionFund"
+
+$xmlFiles = Get-ChildItem "$inPath\PFR*SPIS*.xml"
+if (($xmlFiles | Measure-Object).count -eq 0) {
+    Write-Log -EntryType Error -Message "Xml-файлы в $inPath не найдены!"
+    exit
+}
+
+$info = @()
+
+ForEach ($xmlFile in $xmlFiles) {
+    Try {        
+        $info += parseXML -xmlFile $xmlFile
+    }
+    Catch {
+        Write-Log -EntryType Error $PSItem.ToString()
+    }  
+}
+
+$info = addIterator -data $info
+
+$info | Format-Table
+
+Try {        
+    exportCSV -data $info -fileName $csvFile
+}
+Catch {
+    Write-Log -EntryType Error $PSItem.ToString()
+}  
+
+convertCsv -fileName $csvFile
+
+Write-Log -EntryType Information -Message "Конец работы pensionFund"
+
+Stop-FileLog
+Stop-HostLog
